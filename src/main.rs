52a696d58abdf408e92ec2b4764c9d1bd13c2210
Ian D. Scott <ian@perebruin.com>
2015-08-21 21:01:36 -0700

    Port to irc library
    Has more functionality

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 extern crate regex;
-extern crate irsc;
-extern crate openssl;
+extern crate irc;
 mod github;
 
 use regex::Regex;
-use openssl::ssl::{ Ssl, SslContext, SslMethod };
-use irsc::client::Client;
+use std::default::Default;
+use irc::client::prelude::*;
 
 fn parse_post(post: &str) -> Option<String> {
     let postregex = Regex::new(r"(\S+)/(\S+)#(\d+)").unwrap();
@@ -25,52 +24,47 @@ fn main() {
     let password = args.next().expect("No password provided (argument 2)");
     println!("IRC BOT\nUsername: {}\nPassword: {}\n", username, password);
 
-    println!("Connecting to server...");
-    let mut ircclient = irsc::client::OwnedClient::new();
-    let ssl = Ssl::new(&SslContext::new(SslMethod::Tlsv1).unwrap()).unwrap();
-    ircclient.connect_ssl("irc.freenode.org", 6697, ssl);
-    println!("Registering nick...");
-    ircclient.register(&username, &username,
-                       "Rust Powered IRC Bot", Some(&password));
+    let config = Config {
+        nickname: Some(username.clone()),
+        password: Some(password),
+        server: Some(format!("irc.freenode.org")),
+        channels: Some(vec![format!("#idstest1024")]),
+        port: Some(6697),
+        use_ssl: Some(true),
+        .. Default::default()
+    };
 
-    let mut shared = ircclient.into_shared();
+    let server = IrcServer::from_config(config).unwrap();
+    server.identify().unwrap();
 
-    let _a = shared.commands()
-        .map(move |(mut cl, msg, c)| {
-            if let irsc::command::Command::PRIVMSG(to, content) = c {
-                let from = msg.ident().unwrap().nickname;
+    for message in server.iter() {
+        let message = message.unwrap();
+        if &message.command[..] == "PRIVMSG" {
+            let from = message.get_source_nickname().unwrap().to_owned();
+            let to = message.args[0].to_owned();
+            let content = message.suffix.unwrap();
 
-                if to == username && &from == "ids1024" {
-                    let mut words = content.split_whitespace();
-                    let command = words.next().unwrap_or("");
-                    let parameter = words.next().unwrap_or("");
-                    match command {
-                        "join" => {
-                            println!("Joining '{}'...", parameter);
-                            cl.join(parameter, None);
-                        }
-                        _ => {},
+            if to == username && from == "ids1024" {
+                let mut words = content.split_whitespace();
+                let command = words.next().unwrap_or("");
+                let parameter = words.next().unwrap_or("");
+                match command {
+                    "join" => {
+                        server.send_join(parameter).unwrap();
                     }
+                    _ => {},
                 }
-
-                match parse_post(&content) {
-                    Some(x) => {
-                        for line in x.lines() {
-                            cl.msg(&to, &line);
-                        }
-                    },
-                    None => {}
-                };
             }
-        });
 
-    let _b = shared.replies()
-        .map(|(mut cl, _msg, r)| {
-            if let irsc::reply::Reply::RPL_WELCOME(_) = r {
-                println!("Joining channel(s)...");
-                cl.join("#idstest1024", None);
-            }
-        });
+            match parse_post(&content) {
+                Some(x) => {
+                    for line in x.lines() {
+                        server.send_privmsg(&to, &line).unwrap();
+                    }
+                },
+                None => {}
+            };
+        }
+    }
 
-    shared.listen_with_events();
 }
