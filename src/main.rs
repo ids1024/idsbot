2a1233b1b8dc42170c32dc9791e299a45aa62a71
Ian D. Scott <ian@perebruin.com>
2015-08-28 16:43:46 -0700

    Use if let instead of match for more consistency

--- src/main.rs
+++ src/main.rs
@@ -27,15 +27,14 @@ fn parse_post(post: &str) -> Option<String> {
     }
 
     let issueregex = Regex::new(r"([^ ()]+)/([^ ()]+)#(\d+)").unwrap();
-    let cap = match issueregex.captures(post) {
-        Some(x) => x,
-        None => return None
-    };
-    let user = cap.at(1).unwrap();
-    let repo = cap.at(2).unwrap();
-    let number = cap.at(3).unwrap();
 
-    github::get_display_text(user, repo, number).ok()
+    if let Some(cap) = issueregex.captures(post) {
+        let user = cap.at(1).unwrap();
+        let repo = cap.at(2).unwrap();
+        let number = cap.at(3).unwrap();
+        return github::get_display_text(user, repo, number).ok()
+    }
+    None
 }
 
 fn handle_message(server: &NetIrcServer, from: &str, to: &str, message: &str) {
